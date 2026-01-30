855558ae902b97c6a24a810338153ccc3e65ae88
jaysurani18 <jaysurani008@gmail.com>
2026-01-30 11:39:49 +0530

    Add project files.

---
+++ Controllers/LoanController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPP.Controllers
+{
+    public class LoanController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult ViewAll()
+        {
+
+            return View();
+        }
+        public IActionResult NewApplication()
+        {
+
+            return View();
+        }
+        public IActionResult UpdateApplication()
+        {
+
+            return View();
+        }
+        public IActionResult DiscardApplication()
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ Models/LoanModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WEBAPP.Models
+{
+    public class LoanModel
+    {
+        public int LoanNo { get; set; }
+        public int AccountNo{ get; set; }
+        public string LoanCategory{ get; set; }
+        public string LoanType { get; set; }
+        public string IssueDate { get; set; }
+        public int Amount { get; set; }
+        public string CurrentAddress { get; set; }
+        public string LoanRemark { get; set; } 
+    }
+}
